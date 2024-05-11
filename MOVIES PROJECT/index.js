5de89fb2337ab6b0e284439043b7ac2ac9a0b4a9
Eltagi Osman <153488168+EltagiOSM@users.noreply.github.com>
2024-05-11 19:27:02 +0600

    Add files via upload

---
+++ MOVIES PROJECT/index.js
@@ -0,0 +1,52 @@
+function background0(){
+    document.querySelector(".continer").style.background= "url(images/onepiecebg.jpg)" ;
+    document.getElementById("AnimeName").innerHTML="One Piece";
+    document.getElementById("AnimeName").style.color="#000";
+}
+function background1(){
+    document.querySelector(".continer").style.background= "url('images/attack-bg1.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Attack On Taitan";
+    document.getElementById("AnimeName").style.color="aqua";
+}
+function background2(){
+    document.querySelector(".continer").style.background= "url('images/black-clover-bg1.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Black Clover";
+    document.getElementById("AnimeName").style.color="aqua";
+}
+function background3(){
+    document.querySelector(".continer").style.background= "url('images/nnar.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Naruto";
+    document.getElementById("AnimeName").style.color="blue";
+}
+function background4(){
+    document.querySelector(".continer").style.background= "url('images/hunter-bg.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Naruto";
+    document.getElementById("AnimeName").style.color="blue";
+}
+function background5(){
+    document.querySelector(".continer").style.background= "url('images/bleach-bg.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Naruto";
+    document.getElementById("AnimeName").style.color;
+}
+function background6(){
+    document.querySelector(".continer").style.background= "url('images/dragon-bg.jpg')" ;
+    document.getElementById("AnimeName").innerHTML="Naruto";
+    document.getElementById("AnimeName").style.color="blue";
+}
+function bord(){
+    document.getElementById("bord").style.borderColor="white";
+}
+function colors(){
+   //document.getElementById("about").style.color="red";
+   // document.getElementById("about").style.background="#000";
+    
+}
+function change(){
+    document.querySelector(".continer").style.background="url('images/bleach-bg.jpg')";
+}
+function change1(){
+    document.querySelector(".continer").style.background="url('images/hunter-bg.jpg')";
+}
+function show(){
+   
+}
